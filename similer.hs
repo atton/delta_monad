5ccf39fc3540bad91e6cd4e8e1007038185bc049
atton <e115763@gmail.com>
2014-08-31 18:41:02 +0900

    Reduce definition

--- similer.hs
+++ similer.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE FlexibleContexts #-}
-data Similer a f b = Similer a (a -> b) b
+data Similer a b = Similer a (a -> b) b
 
-instance Functor (Similer a f) where
+instance Functor (Similer a) where
   fmap g (Similer a f b) = Similer a (g . f) $ g b
 
-eta :: a -> Similer a (a -> a) a
+eta :: a -> Similer a a
 eta a = Similer a id a
 
 --mu :: (Eq (Similer b (b -> c) c)) => Similer a (a -> (Similer b (b -> c) c)) (Similer b (b -> c) c) -> Similer b (b -> c) c
-mu :: (Eq (Similer b (b -> c) c)) => Similer a (a -> (Similer b (b -> c) c)) (Similer b (b -> c) c) -> Similer b (b -> c) c
+mu :: (Eq (Similer b c)) => Similer a (Similer b c) -> Similer b c
 mu (Similer a f b) = if ((f a) == b) then b else undefined
