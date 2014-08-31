9f3c398804237a1bda53fe1ecfbf8eee86e690e2
atton <e115763@gmail.com>
2014-08-31 19:21:42 +0900

    Define >>= for Similer

--- similer.hs
+++ similer.hs
@@ -1,12 +1,18 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 data Similer a b = Similer a (a -> b) b
 
 instance Functor (Similer a) where
   fmap g (Similer a f b) = Similer a (g . f) $ g b
 
+same :: (Eq a) => Similer a b -> Similer a b -> Bool
+same (Similer a _ _ ) (Similer b _ _) = a == b
+
 eta :: a -> Similer a a
 eta a = Similer a id a
 
---mu :: (Eq (Similer b (b -> c) c)) => Similer a (a -> (Similer b (b -> c) c)) (Similer b (b -> c) c) -> Similer b (b -> c) c
-mu :: (Eq (Similer b c)) => Similer a (Similer b c) -> Similer b c
-mu (Similer a f b) = if ((f a) == b) then b else undefined
+mu :: (Eq b) => Similer a (Similer b c) -> Similer b c
+mu (Similer a f b) = if (same (f a) b) then b else undefined
+
+instance (Eq a) => Monad (Similer a) where
+        --return x = Similer x id x
+        s >>= f  = mu (fmap f s)
