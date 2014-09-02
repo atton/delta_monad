92418585a6e7f5e22bf72cdc4e6b5253d3cf4544
atton <e115763@gmail.com>
2014-09-02 11:02:00 +0900

    Rename some functions

--- similer.hs
+++ similer.hs
@@ -4,14 +4,14 @@ data Similer a b = Similer a (a -> b) b
 instance Functor (Similer a) where
   fmap g (Similer a f b) = Similer a (g . f) $ g b
 
-same :: (Eq a) => Similer a b -> Similer a b -> Bool
-same (Similer a _ _ ) (Similer b _ _) = a == b
+eq :: (Eq a) => Similer a b -> Similer a b -> Bool
+eq (Similer a _ _ ) (Similer b _ _) = a == b
 
 eta :: a -> Similer a a
 eta a = Similer a id a
 
 mu :: (Eq b) => Similer a (Similer b c) -> Similer b c
-mu (Similer a f b) = if (same (f a) b) then b else undefined
+mu (Similer a f b) = if (eq (f a) b) then b else undefined
 
 double :: Int -> Similer Int Int
 double x = Similer (2 * x) id (2 * x)
@@ -22,5 +22,5 @@ twicePlus x = Similer x double (Similer (x + x) id $ x + x)
 plusTwo :: Int -> Similer Int (Similer Int Int)
 plusTwo x = Similer x double (Similer (x + 2) id (x + 2))
 
-hoge :: Eq b => Similer a b -> b
-hoge (Similer x f y) = if (f x) == y then y else undefined
+same :: Eq b => Similer a b -> b
+same (Similer x f y) = if (f x) == y then y else undefined
