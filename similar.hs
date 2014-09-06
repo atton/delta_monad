622b4b21899b2a1a4f973d79c75f5f4b37de4c71
atton <e115763@gmail.com>
2014-09-06 11:45:32 +0900

    Define fmap and mu

--- similar.hs
+++ similar.hs
@@ -1,34 +1,39 @@
-data Similar a = Single a | Similar a (Similar a)
-
-same :: (Eq a) => Similar a -> a
-same (Single x)    = x
-same (Similar x s) = if x == (same s) then x else undefined
+data Similar a = Single a | Similar a (Similar a) deriving (Show)
 
 instance (Eq a) => Eq (Similar a) where
     s == ss = (same s) == (same ss)
+
+same :: (Eq a) => Similar a -> a
+same (Single x)    = x
+same (Similar x s) = if x == (same s) then x else (error "same")
+
 
 instance Functor Similar where
    fmap f (Single a)    = Single (f a)
    fmap f (Similar a s) = Similar (f a) (fmap f s)
 
+mu :: (Eq a) => (Similar (Similar a)) -> Similar a
+mu (Single x)                 = x
+mu (Similar (Single x) s)     = Similar x (mu s)
+mu (Similar s ss) = Similar (same s) (mu ss)
+
 {-
-
-mu :: (Eq a) => Similar (Similar a) -> Similar a
-mu (Similar a f b) = if ((f a) == b) then b else undefined
-
-similar :: (Eq a) => (a -> a) -> (a -> a) -> a -> a
-similar f g x = same $ Similar x g (f x)
-
+instance Monad Similar where
+    return              = Single
+    (Single x)    >>= f = f x
+    (Similar x s) >>= f = mu $ Similar (f x) (fmap f s)
+-}
 
 
-double :: Int -> Int
-double x = (2 * x)
 
-twicePlus :: Int -> Int
-twicePlus x = x + x
+double :: Int -> Similar Int
+double x = Single (2 * x)
 
-plusTwo :: Int -> Int
-plusTwo x = x + 2
+twicePlus :: Int -> Similar Int
+twicePlus x = Similar (x + x) (double x)
+
+plusTwo :: Int -> Similar Int
+plusTwo x = Similar (x + 2) (double x)
 
 -- samples
 
@@ -43,4 +48,3 @@ plusTwo x = x + 2
 *** Exception: Prelude.undefined
 -}
 
--}
