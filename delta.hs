fdbfb2c47206c10b1b4219940dea54d5de78f92c
atton <e115763@gmail.com>
2014-11-22 12:34:06 +0900

    Mini fixes

--- delta.hs
+++ delta.hs
@@ -1,19 +1,25 @@
 import Control.Applicative
 import Data.Numbers.Primes -- $ cabal install primes
 
+-- delta definition
+
 data Delta a = Mono a | Delta a (Delta a) deriving Show
+
+-- basic functions
 
 deltaAppend :: Delta a -> Delta a -> Delta a
 deltaAppend (Mono x) d     = Delta x d
 deltaAppend (Delta x d) ds = Delta x (deltaAppend d ds)
 
 headDelta :: Delta a -> Delta a
-headDelta d@(Mono  _)   = d
+headDelta d@(Mono  _) = d
 headDelta (Delta x _) = Mono x
 
 tailDelta :: Delta a -> Delta a
 tailDelta d@(Mono _)   = d
 tailDelta (Delta _ ds) = ds
+
+-- instance definitions
 
 instance Functor Delta where
     fmap f (Mono x)    = Mono  (f x)
@@ -26,17 +32,22 @@ instance Applicative Delta where
     (Delta f df) <*> d@(Mono x)  = Delta (f x) (df <*> d)
     (Delta f df) <*> (Delta x d) = Delta (f x) (df <*> d)
 
-
 instance Monad Delta where
     return x = Mono x
-    (Mono x)    >>= f  = f x
-    (Delta x d) >>= f  = (headDelta (f x)) `deltaAppend` (d >>= (tailDelta . f))
+    (Mono x)    >>= f = f x
+    (Delta x d) >>= f = (headDelta (f x)) `deltaAppend` (d >>= (tailDelta . f))
+
+
+-- utils
 
 returnS :: (Show s) => s -> Delta s
 returnS x = Mono x
 
 returnSS :: (Show s) => s -> s -> Delta s
 returnSS x y = (returnS x) `deltaAppend` (returnS y)
+
+deltaFromList :: [a] -> Delta a
+deltaFromList = (foldl1 deltaAppend) . (fmap return)
 
 
 -- samples
